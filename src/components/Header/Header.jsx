75da0e971b53a0810690636d9ef28cef177820f3
Greger Gundersen <g.gundersen@netbots.no>
2023-03-16 20:26:58 +0100

    Added header 2 - Electric boogaloo

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { HeaderStyled } from './Header.styled'
+
+const Header = () => {
+  return (
+    <HeaderStyled>Header</HeaderStyled>
+  )
+}
+
+export default Header
