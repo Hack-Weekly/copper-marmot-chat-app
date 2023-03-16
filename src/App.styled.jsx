23e3a9600209dfa553cd43ba12f721551d1ffc52
Greger Gundersen <g.gundersen@netbots.no>
2023-03-16 20:03:19 +0100

    Added dependencies, set up file structure

---
+++ src/App.styled.jsx
@@ -0,0 +1,15 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        h1, h2, h3, h4, h5{
+
+        }
+        p, span, a{
+
+        }
+    }
+`;
