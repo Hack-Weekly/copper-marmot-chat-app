98f7ec80b698eeffb2b60a3456f7dc40c2f2ed44
Greger Gundersen <g.gundersen@netbots.no>
2023-03-16 20:05:46 +0100

    Added header

---
+++ src/components/Header/Header.styled.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+
+export const HeaderStyled = styled.div`
+  ul {
+    list-style: none;
+    font-size: 1.8rem;
+    margin-left: 0;
+    li {
+      padding: 0 12px;
+    }
+  }
+`;
