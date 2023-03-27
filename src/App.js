208eb4d933b430fe79fe917171055bff90430307
Shai Raz <shayy9999@gmail.com>
2023-03-27 19:35:46 +0300

    remove router for gh pages

--- src/App.js
+++ src/App.js
@@ -11,12 +11,7 @@ function App() {
     <ThemeProvider theme={theme}>
       <StyledApp>
         <GlobalStyle />
-        <Router>
-          <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/chat" element={<Chat />} />
-          </Routes>
-        </Router>
+        <Main />
       </StyledApp>
     </ThemeProvider>
   );
